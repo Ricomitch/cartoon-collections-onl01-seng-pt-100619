ab188a96a772c182d6449ecbc8348caa792605fb
Rico Mitchell <ricomitch@icloud.com>
2019-10-20 04:56:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,3 +14,30 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+def roll_call_dwarves(names)
+call_dwarves= []
+  names.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+  end
+  # call_dwarves
+end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|name| name.capitalize + "!"}
+end
+
+def long_planeteer_calls(calls)
+  if calls.any? { |name| name.length > 4}
+    true
+    else calls.any? { |name| name.length < 4}
+    false
+  end
+end
+
+def find_the_cheese(array)
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |cheese| cheese_types.include?(cheese) }
+  
+end
